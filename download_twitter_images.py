822a08289150a9d8f3770001cf033072ddf378af
ionic-bond <ionicbond3@gmail.com>
2024-02-16 02:36:17 +0800

    Support GraphQL API

--- download_twitter_images.py
+++ download_twitter_images.py
@@ -4,10 +4,14 @@ import click
 import json
 import logging
 import os
+import time
+from collections import deque
 
 import requests
 from requests.cookies import RequestsCookieJar
 from tweepy_authlib import CookieSessionUserHandler
+
+from graphql_api import GraphqlAPI
 
 
 @click.group()
@@ -48,40 +52,114 @@ def get_proxies():
     return proxies
 
 
-def send_get_request(url: str, params: dict = {}):
-    auth_handler = load_auth_handler(cookie_path)
+def get_headers(headers, cookies) -> dict:
+    authed_headers = headers | {
+        'cookie': '; '.join(f'{k}={v}' for k, v in cookies.items()),
+        'referer': 'https://twitter.com/',
+        'x-csrf-token': cookies.get('ct0', ''),
+        'x-guest-token': cookies.get('guest_token', ''),
+        'x-twitter-auth-type': 'OAuth2Session' if cookies.get('auth_token') else '',
+        'x-twitter-active-user': 'yes',
+        'x-twitter-client-language': 'en',
+    }
+    return dict(sorted({k.lower(): v for k, v in authed_headers.items()}.items()))
+
+
+def build_params(params: dict) -> dict:
+    return {k: json.dumps(v) for k, v in params.items()}
+
+
+def find_all(obj: any, key: str) -> list:
+    # DFS
+    def dfs(obj: any, key: str, res: list) -> list:
+        if not obj:
+            return res
+        if isinstance(obj, list):
+            for e in obj:
+                res.extend(dfs(e, key, []))
+            return res
+        if isinstance(obj, dict):
+            if key in obj:
+                res.append(obj[key])
+            for v in obj.values():
+                res.extend(dfs(v, key, []))
+        return res
+
+    return dfs(obj, key, [])
+
+
+def find_one(obj: any, key: str) -> any:
+    # BFS
+    que = deque([obj])
+    while len(que):
+        obj = que.popleft()
+        if isinstance(obj, list):
+            que.extend(obj)
+        if isinstance(obj, dict):
+            if key in obj:
+                return obj[key]
+            for v in obj.values():
+                que.append(v)
+    return None
+
+
+def get_cursor(obj: any) -> str:
+    entries = find_one(obj, 'entries')
+    for entry in entries:
+        entry_id = entry.get('entryId', '')
+        if entry_id.startswith('cursor-bottom'):
+            return entry.get('content', {}).get('value', '')
+
+
+def send_get_request(api_name: str, params: dict = {}):
+    with open(cookie_path, 'r') as f:
+        cookies = json.load(f)
+    url, _, headers, features = GraphqlAPI.get_api_data(api_name)
+    headers = get_headers(headers, cookies)
+    params = build_params({"variables": params, "features": features})
     response = requests.request("GET",
                                 url,
                                 params=params,
-                                auth=auth_handler.apply_auth(),
+                                headers=headers,
                                 proxies=get_proxies())
     while response.status_code != 200:
         logging.error("Request returned an error: {} {}".format(response.status_code,
                                                                 response.text))
+        time.sleep(5)
         response = requests.request("GET",
                                     url,
                                     params=params,
-                                    auth=auth_handler.apply_auth(),
+                                    headers=headers,
                                     proxies=get_proxies())
     return response.json()
 
 
+def get_id_by_username(username: str):
+    api_name = 'UserByScreenName'
+    params = {'screen_name': username}
+    json_response = send_get_request(api_name, params)
+    while json_response is None:
+        time.sleep(10)
+        json_response = send_get_request(api_name, params)
+    return find_one(json_response, 'rest_id')
+
+
 def get_favorite_tweets(username: str):
     result = []
-    url = "https://api.twitter.com/1.1/favorites/list.json?count=200&screen_name={}".format(
-        username)
-    favorite_tweets = send_get_request(url)
+    user_id = get_id_by_username(username)
+    api_name = 'Likes'
+    params = {'userId': user_id, 'includePromotedContent': True, 'count': 1000}
+    json_response = send_get_request(api_name, params)
+    favorite_tweets = find_all(json_response, 'tweet_results')
     result.extend(favorite_tweets)
-    for _ in range(4):
-        if not favorite_tweets:
+    while len(result) < 500:
+        cursor = get_cursor(json_response)
+        if not cursor or cursor.startswith('-1|') or cursor.startswith('0|'):
             break
-        min_id = min(favorite_tweet['id'] for favorite_tweet in favorite_tweets)
-        print(min_id)
-        url = "https://api.twitter.com/1.1/favorites/list.json?count=200&screen_name={}&max_id={}".format(
-            username, min_id - 1)
-        favorite_tweets = send_get_request(url)
+        params['cursor'] = cursor
+        json_response = send_get_request(api_name, params)
+        favorite_tweets = find_all(json_response, 'tweet_results')
         result.extend(favorite_tweets)
-    print(len(result))
     return result
 
 
@@ -93,7 +171,7 @@ def read_prossesed_ids(username: str):
         return processed_ids
     with open(filename, 'r') as f:
         for processed_id in f:
-            processed_ids.add(int(processed_id.rstrip('\r\n')))
+            processed_ids.add(processed_id.rstrip('\r\n'))
     return processed_ids
 
 
@@ -144,6 +222,8 @@ def download_user_like_images(username, auth_cookie_path, output_dir, scan_dirs,
     logging.basicConfig(filename=log_path, format='%(asctime)s - %(message)s', level=logging.INFO)
     os.makedirs(output_dir, exist_ok=True)
 
+    GraphqlAPI.init()
+
     global cookie_path
     cookie_path = auth_cookie_path
 
@@ -161,12 +241,17 @@ def download_user_like_images(username, auth_cookie_path, output_dir, scan_dirs,
 
     favorite_tweets = get_favorite_tweets(username)
     for favorite_tweet in favorite_tweets:
-        if favorite_tweet['user']['screen_name'] in exclude_users:
+        user = find_one(favorite_tweet, 'user_results')
+        like_username = find_one(user, 'screen_name')
+        if like_username in exclude_users:
             continue
-        if favorite_tweet['id'] in processed_ids:
+        if find_one(favorite_tweet, 'rest_id') in processed_ids:
             continue
-        write_processed_id(username, favorite_tweet['id'])
-        medias = favorite_tweet.get('extended_entities', {}).get('media', [])
+        write_processed_id(username, find_one(favorite_tweet, 'rest_id'))
+        extended_entities = find_one(favorite_tweet, 'extended_entities')
+        if not extended_entities:
+            continue
+        medias = extended_entities.get('media', [])
         for media in medias:
             media_name = media['media_url_https'].split('/')[-1]
             if media_name in existed_images:
@@ -184,18 +269,21 @@ def download_user_like_images(username, auth_cookie_path, output_dir, scan_dirs,
 
 def get_tweets(username: str):
     result = []
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name={}&trim_user=false&include_rts=false".format(
-        username)
-    tweets = send_get_request(url)
+    user_id = get_id_by_username(username)
+    api_name = 'UserMedia'
+    params = {'userId': user_id, 'includePromotedContent': True, 'withVoice': True, 'count': 1000}
+    json_response = send_get_request(api_name, params)
+    tweets = find_all(json_response, 'tweet_results')
     result.extend(tweets)
-    while len(tweets):
-        min_id = min(tweet['id'] for tweet in tweets)
-        print(min_id)
-        url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name={}&max_id={}&trim_user=false&include_rts=false".format(
-            username, min_id - 1)
-        tweets = send_get_request(url)
+    while tweets:
+        cursor = get_cursor(json_response)
+        if not cursor or cursor.startswith('-1|') or cursor.startswith('0|'):
+            break
+        params['cursor'] = cursor
+        json_response = send_get_request(api_name, params)
+        tweets = find_all(json_response, 'tweet_results')
         result.extend(tweets)
-    print(len(result))
+        print(len(result))
     return result
 
 
@@ -213,11 +301,16 @@ def download_user_tweet_images(username, auth_cookie_path, output_dir, log_path)
     global cookie_path
     cookie_path = auth_cookie_path
 
+    GraphqlAPI.init()
+
     image_urls = []
 
     tweets = get_tweets(username)
     for tweet in tweets:
-        medias = tweet.get('extended_entities', {}).get('media', [])
+        extended_entities = find_one(tweet, 'extended_entities')
+        if not extended_entities:
+            continue
+        medias = extended_entities.get('media', [])
         for media in medias:
             media_type = media.get('type', '')
             if media_type != 'photo':
