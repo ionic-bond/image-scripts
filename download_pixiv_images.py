a249dac667df7b0e69bc8205faf8ed185c05e693
Auto YAPF <github-actions@github.com>
2025-04-06 12:11:10 +0000

    Auto YAPF with Google python style

--- download_pixiv_images.py
+++ download_pixiv_images.py
@@ -92,10 +92,15 @@ def download_image(output_dir: str, image_url: str):
         return
     print('Downloading image {} to {}'.format(image_url, output_path))
     logging.info('Downloading image {} to {}'.format(image_url, output_path))
-    r = requests.get(image_url,
-                     proxies=get_proxies(),
-                     headers={'Referer': 'https://www.pixiv.net/',
-                              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'})
+    r = requests.get(
+        image_url,
+        proxies=get_proxies(),
+        headers={
+            'Referer':
+                'https://www.pixiv.net/',
+            'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
+        })
     with open(output_path, "wb") as f:
         f.write(r.content)
 
